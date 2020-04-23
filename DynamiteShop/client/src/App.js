d1310a621882ffc18f12d1189d2bb8a482079b4d
bambojr9 <61042675+bambojr9@users.noreply.github.com>
2020-04-23 16:34:35 +0700

    nothing

--- DynamiteShop/client/src/App.js
+++ DynamiteShop/client/src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.scss';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import AllProducts from './Components/AllProducts/AllProducts';
+import Home from './Components/Home/Home';
+import EEEE from './Components/Error/EEEE';
 
 function App() {
 	return (
 		<Router>
 			<Header></Header>
+			<Switch>
+				<Route exact path={['/' , '/Home']} component={Home} />
+				<Route path="/all-products" component={AllProducts} />
+				<Route component={EEEE} />
+			</Switch>
 			<Footer></Footer>
 		</Router>
 	);
